30bfd73efc64b4f366813050ab6c2cee11663b7c
Amit yadav <Amit.yadav@gemsessence.com>
2022-09-08 18:31:38 +0530

    added edit usr accnt

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,11 @@ class User < ApplicationRecord
                   format: { with: /\S+@\S+/ },
                   uniqueness: { case_sensitive: false }
 
-  validates :password, length: { minimum: 6, allow_blank: true }    
+  validates :password, length: { minimum: 6, allow_blank: true }
+  
+  validates :username, presence: true,
+                     format: { with: /\A[A-Z0-9]+\z/i },
+                     uniqueness: { case_sensitive: false }
   
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
