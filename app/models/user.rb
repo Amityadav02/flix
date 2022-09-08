214dc8ad91d51729fbe95d9ee60b97e8f977500b
Amit yadav <Amit.yadav@gemsessence.com>
2022-09-08 14:46:23 +0530

    added account model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+
+  validates :email, format: { with: /\S+@\S+/ }
+
+  validates :email, presence: true,
+                  format: { with: /\S+@\S+/ },
+                  uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 7, allow_blank: true }               
+
+end
